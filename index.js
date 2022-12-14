efa2e63cb56015274ff38173dc556bedf93cc414
Parth Khatri <parthkhatri.it@gmail.com>
2022-12-14 12:48:22 +0000

    Var Let Const

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Parth Khatri 123")
-var a = 67;
-console.log(a)
-
-a = "Parth";
-console.log(a);
-
-//let var = 1; // not allowed
-//let 8parth = 1; // Not allowed
---
+++ index2.js
@@ -0,0 +1,13 @@
+console.log('JS Var Let Const')
+
+var a = 45;
+var b = "Parth";
+var c = null;
+var d = undefined;
+
+{
+  let b = "this"
+  console.log(b)
+}
+
+console.log(b)
